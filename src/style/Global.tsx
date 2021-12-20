afc8a7922bb6fb06a56a019c562d9d0377345086
mykyta <mykyta.dev11@gmail.com>
2021-12-20 16:03:26 +0200

    fix(theme): change default theme type

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,10 +1,11 @@
+import { PancakeTheme } from '@pancakeswap/uikit'
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
-import { Theme } from './theme'
+// import { Theme } from './theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends Theme {}
+  export interface DefaultTheme extends PancakeTheme {}
 }
 
 const GlobalStyle = createGlobalStyle`
